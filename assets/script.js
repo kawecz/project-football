058af3f0806ba2d258e36f5abdba7c7207150ed8
Kawê Cezar <kawecezarjesus@gmail.com>
2024-08-17 18:47:58 -0500

    configurando menu hamburguer

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
+function mudouTamanho(){
+    if (window.innerWidth >= 670) {
+        navmenu.style.display = 'block'
+    } else {
+        navmenu.style.display ='none'
+    }
+}
+
+
 function menuClick() {
-    alert('Menu em manutenção')
+    if (navmenu.style.display == 'none') {
+        navmenu.style.display = 'block'
+    } else {
+        navmenu.style.display = 'none'
+    }
 }
